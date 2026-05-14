f80154a1666677251a17a9f2425fbd63cf9acfe8
nazhat <nazhathafidzaljauzi2007@gmail.com>
2026-05-14 22:10:16 +0700

    3.peek

--- stackusinglinkedlist.cpp
+++ stackusinglinkedlist.cpp
@@ -80,7 +80,7 @@ int main()
     {
         cout << "1. Push\n";
         cout << "2. Pop\n"; 
-        cout << "3. Peek\n";
+        cout << "3. Peek\n";  
         cout << "4. Exit\n";
         cout << "Enter your choice: ";
         cin >> choice;
