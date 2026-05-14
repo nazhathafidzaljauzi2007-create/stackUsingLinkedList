c0ff38e5d2879816f6bcdd2eea4974d3e1b2fa43
nazhat <nazhathafidzaljauzi2007@gmail.com>
2026-05-14 22:02:50 +0700

    class stack

--- stackusinglinkedlist.cpp
+++ stackusinglinkedlist.cpp
@@ -13,3 +13,59 @@ public:
     }
 };
 
+// stack class
+class Stack 
+{
+private:
+    Node* top;
+public:
+    Stack() 
+    {
+        top = NULL;
+    }
+    int push(int value)
+    {
+        Node* newNode = new Node();
+        newNode->data = value;
+        newNode->next = top;
+        top = newNode;
+        cout << "Push value: " << value << endl;
+        return value;
+    }
+    
+    void pop() 
+    {
+        if (isEmpty()) 
+        {
+            cout << "Stack is empty." << endl;
+        }
+
+        Node* temp = top;
+        top = top->next;
+        cout << "Popped value: " << top->data << endl;
+    }
+
+    void peek()
+    {
+        if (top == NULL) 
+        {
+            cout << "List is empty." << endl;
+        }
+        else 
+        {
+            Node *current = top;
+            while (current != NULL) 
+            {
+                cout << current->data << " " << endl;
+                current = current->next;
+            }
+            cout << endl;
+        }
+    }
+
+    bool isEmpty() 
+    {
+        return top == NULL;
+    }
+};
+
