5126d0921d043729bbf4dc7d823e5dd028dad4f9
nazhat <nazhathafidzaljauzi2007@gmail.com>
2026-05-14 22:09:45 +0700

    2.pop

--- stackusinglinkedlist.cpp
+++ stackusinglinkedlist.cpp
@@ -79,7 +79,7 @@ int main()
     while (choice != 5)
     {
         cout << "1. Push\n";
-        cout << "2. Pop\n";
+        cout << "2. Pop\n"; 
         cout << "3. Peek\n";
         cout << "4. Exit\n";
         cout << "Enter your choice: ";
